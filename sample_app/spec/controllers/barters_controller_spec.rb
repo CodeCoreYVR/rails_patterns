252bf84da64e162c6b14b137a0e9b96b4f645e58
Adam Cooper <adam.cooper@gmail.com>
2014-02-28 11:11:17 -0800

    move specs from controller to model

--- sample_app/spec/controllers/barters_controller_spec.rb
+++ sample_app/spec/controllers/barters_controller_spec.rb
@@ -3,43 +3,20 @@ require 'spec_helper'
 describe BartersController do
 
   describe "#create" do
+
     let(:create_params) {
       { barter: FactoryGirl.attributes_for(:barter) }
     }
+
     def perform_post(params = {})
       post(:create, create_params.merge(params))
     end
+
     it "creates a barter from the params" do
       expect {
         perform_post
       }.to change{ Barter.count }.by(1)
     end
-    context "creating a new barter" do
-      it "notifies the submitter that the barter was posted" do
-        expect(UserMailer).to receive(:barter_posted).and_return(double('mailer').as_null_object)
-        perform_post
-      end
-    end
-
-    context "creating a response barter" do
-      let!(:original_barter) { create(:barter) }
-      let(:create_params) {
-        { barter: attributes_for(:barter).merge(barter_id: original_barter.id) }
-      }
-      it "notifies the submitter that the barter was posted" do
-        original_barter # ensure it's created before the expectation
-        expect(UserMailer).to receive(:barter_posted).and_return(double('mailer').as_null_object)
-        perform_post
-      end
-
-      it "notifies the original submitter that there is a response" do
-        expect(UserMailer).to receive(:barter_response).and_return(double('mailer').as_null_object)
-        perform_post
-
-      end
-
-    end
-
   end
 
 end
--- sample_app/spec/models/barter_spec.rb
+++ sample_app/spec/models/barter_spec.rb
@@ -11,4 +11,28 @@ describe Barter do
     end
   end
 
+  describe "notify the submitter" do
+    it "notifies the submitter that their barter was posted to the site" do
+      barter = build(:barter)
+      expect(UserMailer).to receive(:barter_posted).
+        with(barter).
+        and_return(double('mailer').as_null_object)
+
+      barter.save
+    end
+  end
+
+  describe "notify the original submitter when a response is posted" do
+    it "notifies the submitter that their barter was posted to the site" do
+      original_barter = create(:barter)
+      response_barter = build(:barter, barter_id: original_barter.id)
+
+      expect(UserMailer).to receive(:barter_response).
+        with(response_barter, original_barter).
+        and_return(double('mailer').as_null_object)
+
+      response_barter.save
+    end
+  end
+
 end
